ba90aa47fe49cf451a8da8c808ec54df044f4dc3
Wilson Alves <wlsn.ads@gmail.com>
2020-07-19 22:01:06 -0300

    introducao angular

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu Primeiro component com Angular 2</p>
+  `
+})
+export default class MeuPrimeiroComponent {}
